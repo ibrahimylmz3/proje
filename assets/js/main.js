e0c940f9103c18324793578faea066c45fc879f1
İbrahim Yılmaz <ibrahim.yilmaz@iyilmaz.com>
2019-01-15 01:10:42 +0300

    son değişiklikler
    admin tarafında bazı sayfalar tamamlandı. çok fazla kopyaya girdiği için bir kısımdan sonrası bırakıldı.
    login formunda JavsScript ile doğrulama yapıldı sayfa menü bağlantıları güncellendi.

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,24 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+    function kontrol(text) {
+        var username, pass, text ; 
+        username= document.getElementById("k_adi").value.length;
+        pass= document.getElementById("pass").value.length;
+        
+        if (username == 0 || pass == 0) {    
+        text= "Kullanıcı adı veya şifre boş";
+        }
+        else if (pass < 6) {
+        text= "Sifre 6 karakterden kısa";
+        }
+        else if (username < 3 || username > 20) {    
+        text= "Kullanici adi 3-20 karakter arasında olmalıdır"; 
+        }
+        else {    
+        text= "Giriş Başarılı" ;
+        console.log("Profil sayfasına yönlendiriliyorsunuz");
+        window.location.href = "userProfil.html";
+        }
+        document.getElementById("result").innerHTML = text;
+  }
